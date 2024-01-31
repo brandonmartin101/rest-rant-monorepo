81b33915798b4e62c5196104b7b44887ab2788f4
bmartin-dealersocket <bmartin@dealersocket.com>
2024-01-31 16:12:41 -0600

    Sessions activity

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -16,7 +16,32 @@ router.post('/', async (req, res) => {
 			message: 'Invalid email or password',
 		});
 	} else {
+		req.session.userId = user.userId;
 		res.json({ user });
+	}
+});
+
+router.get('/profile', async (req, res) => {
+	console.log(req.session.userId);
+	try {
+		let user = await User.findOne({
+			where: {
+				userId: req.session.userId,
+			},
+		});
+		res.json(user);
+	} catch {
+		res.json(null);
+	}
+});
+
+router.post('/super-important-route', async (req, res) => {
+	if (req.session.userId) {
+		console.log('Do the really super important thing');
+		res.send('Done');
+	} else {
+		console.log('You are not authorized to do the super important thing');
+		res.send('Denied');
 	}
 });
 
